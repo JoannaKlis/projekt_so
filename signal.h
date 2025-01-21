a8c583706b52ef519477dda936afd37ade1e34c3
Joanna Kliś <joannnaklis8@gmail.com>
2025-01-21 22:23:33 +0100

    dodanie 2 sygnałów zarządcy stacji

--- signal.h
+++ signal.h
@@ -3,10 +3,12 @@
 #include "functions.h"
 
 volatile sig_atomic_t running = 1; // flaga kontrolna dla procesu do zakonczenia dzialania programu
+volatile sig_atomic_t signal1 = 1; // flaga dla sygnalu CTRL+K
+volatile sig_atomic_t signal2 = 1; // flaga dla sygnalu CTRL+L
 
 void handle_signal(int signal) // sygnal na przerwanie dzialania
 {
-    printf(COLOR_MAGENTA "PASAZER: Otrzymalem sygnal %d\n" COLOR_RESET, signal);
+    printf(COLOR_PINK "SIGNAL: Otrzymalem sygnal %d\n" COLOR_RESET, signal);
     running = 0; // zatrzymanie dzialania
 }
 
@@ -15,7 +17,7 @@ void *keyboard_signal(void *arg) // odczyt sygnalu z klawiatury
     struct termios oldt, newt;
     if (tcgetattr(STDIN_FILENO, &oldt) == -1) // sprawdzenie bledu tcgetattr
     {
-        perror(COLOR_RED "PASAZER: Blad tcgetattr" COLOR_RESET);
+        perror(COLOR_RED "SIGNAL: Blad tcgetattr" COLOR_RESET);
         return NULL;
     }
 
@@ -24,7 +26,7 @@ void *keyboard_signal(void *arg) // odczyt sygnalu z klawiatury
 
     if (tcsetattr(STDIN_FILENO, TCSANOW, &newt) == -1) // obsluga bledu tcsetattr
     {
-        perror(COLOR_RED "PASAZER: Blad tcsetattr" COLOR_RESET);
+        perror(COLOR_RED "SIGNAL: Blad tcsetattr" COLOR_RESET);
         return NULL;
     }
 
@@ -33,15 +35,27 @@ void *keyboard_signal(void *arg) // odczyt sygnalu z klawiatury
         char ch = getchar();
         if (ch == 14) // ASCII dla CTRL+N
         {
-            printf(COLOR_MAGENTA "PASAZER: Przeslano sygnal CTRL+N. Koniec generowania pasazerow.\n" COLOR_RESET);
+            printf(COLOR_PINK "SIGNAL: Przeslano sygnal CTRL+N. Koniec generowania pasazerow.\n" COLOR_RESET);
             running = 0; // zatrzymanie procesu generowania pasazerow
+            break;
+        }
+        else if (ch == 11)
+        {
+            printf(COLOR_PINK "SIGNAL: Przeslano sygnal CTRL+K. Pociag odjezdza\n" COLOR_RESET);
+            signal1 = 0;
+            break;
+        }
+        else if (ch == 12)
+        {
+            printf(COLOR_PINK "SIGNAL: Przeslano sygnal CTRL+L. Pasazerowie nie moga wejsc\n" COLOR_RESET);
+            signal2 = 0;
             break;
         }
     }
 
     if (tcsetattr(STDIN_FILENO, TCSANOW, &oldt) == -1) // przywrocenie starych ustawien terminala
     {
-        perror(COLOR_RED "PASAZER: Blad przywracania tcsetattr" COLOR_RESET);
+        perror(COLOR_RED "SIGNAL: Blad przywracania tcsetattr" COLOR_RESET);
     }
     return NULL;
 }
@@ -50,7 +64,7 @@ void create_and_start_keyboard_thread(pthread_t *thread) // tworzenie i uruchomi
 {
     if (pthread_create(thread, NULL, keyboard_signal, NULL) != 0)
     {
-        handle_error(COLOR_RED "PASAZER: Blad utworzenia watku" COLOR_RESET);
+        handle_error(COLOR_RED "SIGNAL: Blad utworzenia watku" COLOR_RESET);
     }
 }
 
@@ -58,7 +72,7 @@ void wait_for_keyboard_thread(pthread_t *thread) // oczekiwanie na zakonczenie d
 {
     if (pthread_join(*thread, NULL) != 0)
     {
-        handle_error(COLOR_RED "PASAZER: Blad synchronizacji watku" COLOR_RESET);
+        handle_error(COLOR_RED "SIGNAL: Blad synchronizacji watku" COLOR_RESET);
     }
 }
 
--- station_master.h
+++ station_master.h
@@ -1,5 +1,6 @@
 #ifndef STATION_MASTER_H
 #define STATION_MASTER_H
+#include "signal.h"
 
 void wait_for_child_process(pid_t pid, const char *process_name) // oczekiwanie na zakonczenie procesu potomnego
 {
@@ -27,31 +28,44 @@ void station_master(Data *data, int sem_passengers_bikes, int sem_passengers) //
         printf(COLOR_GREEN "ZARZADCA: Wejscie dla pasazerow bez rowerow odblokowane.\n" COLOR_RESET);
 
         printf(COLOR_CYAN "ZARZADCA: Liczba wszystkich oczekujacych: %d.\n" COLOR_RESET, data->passengers_waiting);
-        sleep(BLOCK_SLEEP*2); // oczekiwanie na wejscie pasazerow
 
-        if (run_for_Ttime())
+        if (signal2 == 0)
+        {
+            printf(COLOR_YELLOW "ZARZADCA: Sygnał blokady wejścia pasażerów (CTRL+L).\n" COLOR_RESET);
+            sleep(1); // tymczasowa blokada wejścia pasażerów na 1 sekundę
+            signal2 = 1; // reset flagi signal2
+        }
+
+        sleep(BLOCK_SLEEP * 2); // oczekiwanie na wejscie pasazerow
+
+        if (signal1 == 0)
+        {
+            printf(COLOR_YELLOW "ZARZADCA: Natychmiastowy odjazd pociągu wymuszony sygnałem (CTRL+K).\n" COLOR_RESET);
+            signal1 = 1; // reset flagi signal1
+        }
+        else if (run_for_Ttime())
         {
             printf(COLOR_CYAN "ZARZADCA: Pociag %d odjezdza ze stacji 1.\n" COLOR_RESET, data->current_train + 1);
-
-            semaphore_wait(sem_passengers_bikes); // blokada wejscia do pociagu dla pasazerow z rowerami
-            printf(COLOR_RED "ZARZADCA: Wejscie dla pasazerow z rowerami zablokowane.\n" COLOR_RESET);
-
-            semaphore_wait(sem_passengers); // blokada wejscia do pociagu dla pasazerow bez rowerami
-            printf(COLOR_RED "ZARZADCA: Wejscie dla pasazerow bez rowerami zablokowane.\n" COLOR_RESET);
-
-            data->free_seat = MAX_PASSENGERS; // aktualizacja liczby miejsc w pociagu
-
-            printf(COLOR_CYAN "ZARZADCA: Pociag %d dotarl na stacje 2.\n" COLOR_RESET, data->current_train + 1);
-
-            sleep(BLOCK_SLEEP*TRAIN_ARRIVAL_TIME); // prowrot ze stacji 2
-
-            data->free_seat = 0; // reset miejsc po powrocie
-            data->free_bike_spots = MAX_BIKES; // reset miejsc dla rowerow
-
-            printf(COLOR_CYAN "ZARZADCA: Pociag %d wrocil na stacje 1.\n" COLOR_RESET, data->current_train + 1);
-
-            data->current_train = (data->current_train + 1) % MAX_TRAINS; // obieg z pociagami
         }
+
+        semaphore_wait(sem_passengers_bikes); // blokada wejscia do pociagu dla pasazerow z rowerami
+        printf(COLOR_RED "ZARZADCA: Wejscie dla pasazerow z rowerami zablokowane.\n" COLOR_RESET);
+
+        semaphore_wait(sem_passengers); // blokada wejscia do pociagu dla pasazerow bez rowerow
+        printf(COLOR_RED "ZARZADCA: Wejscie dla pasazerow bez rowerow zablokowane.\n" COLOR_RESET);
+
+        data->free_seat = MAX_PASSENGERS; // aktualizacja liczby miejsc w pociagu
+
+        printf(COLOR_CYAN "ZARZADCA: Pociag %d dotarl na stacje 2.\n" COLOR_RESET, data->current_train + 1);
+
+        sleep(BLOCK_SLEEP * TRAIN_ARRIVAL_TIME); // powrot ze stacji 2
+
+        data->free_seat = 0; // reset miejsc po powrocie
+        data->free_bike_spots = MAX_BIKES; // reset miejsc dla rowerow
+
+        printf(COLOR_CYAN "ZARZADCA: Pociag %d wrocil na stacje 1.\n" COLOR_RESET, data->current_train + 1);
+
+        data->current_train = (data->current_train + 1) % MAX_TRAINS; // obieg z pociagami
 
         if (data->passengers_waiting == 0 && !data->generating) // sprawdzenie, czy brak oczekujacych pasazerow oraz czy zakonczono generowanie
         {
